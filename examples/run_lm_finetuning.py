eaee86fbaca19f1d054ef795673661e9f6096478
Fabiano <famube@gmail.com>
2022-05-17 13:15:38 -0300

    lm fine tuning

--- examples/run_lm_finetuning.py
+++ examples/run_lm_finetuning.py
@@ -436,8 +436,8 @@ def main():
             nb_tr_examples, nb_tr_steps = 0, 0
             for step, batch in enumerate(tqdm(train_dataloader, desc="Iteration")):
                 batch = tuple(t.to(device) for t in batch)
-                input_ids, input_mask, segment_ids, lm_label_ids, is_next = batch
-                loss = model(input_ids, segment_ids, input_mask, lm_label_ids, is_next)
+                input_ids, input_mask, lm_label_ids = batch
+                loss = model(input_ids=input_ids, input_mask=input_mask, labels=lm_label_ids)
                 if n_gpu > 1:
                     loss = loss.mean() # mean() to average on multi-gpu.
                 if args.gradient_accumulation_steps > 1:
