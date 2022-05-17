58c624add37c0a756ba4e699a2d05844922db0d6
Fabiano <famube@gmail.com>
2022-05-17 12:41:44 -0300

    lm fine tuning

--- examples/run_lm_finetuning_next_sent.py
+++ examples/run_lm_finetuning_next_sent.py
@@ -54,35 +54,69 @@ class BERTDataset(Dataset):
         self.tokenizer = tokenizer
         self.seq_len = seq_len
         self.on_memory = on_memory
+        self.corpus_lines = corpus_lines  # number of non-empty lines in input corpus
         self.corpus_path = corpus_path
         self.encoding = encoding
+        self.current_doc = 0  # to avoid random sentence from same doc
 
         # for loading samples directly from file
         self.sample_counter = 0  # used to keep track of full epochs on file
+        self.line_buffer = None  # keep second sentence of a pair in memory and use as first sentence in next pair
 
         # for loading samples in memory
+        self.current_random_doc = 0
         self.num_docs = 0
+        self.sample_to_doc = [] # map sample index to doc and line
 
         # load samples into memory
         if on_memory:
             self.all_docs = []
+            doc = []
+            self.corpus_lines = 0
             with open(corpus_path, "r", encoding=encoding) as f:
-                for line in tqdm(f, desc="Loading Dataset"):
-                    lin = line.strip()
-                    if len(lin) == 0:
-                        continue
-                    self.all_docs.append(lin)
+                for line in tqdm(f, desc="Loading Dataset", total=corpus_lines):
+                    line = line.strip()
+                    if line == "":
+                        self.all_docs.append(doc)
+                        doc = []
+                        #remove last added sample because there won't be a subsequent line anymore in the doc
+                        self.sample_to_doc.pop()
+                    else:
+                        #store as one sample
+                        sample = {"doc_id": len(self.all_docs),
+                                  "line": len(doc)}
+                        self.sample_to_doc.append(sample)
+                        doc.append(line)
+                        self.corpus_lines = self.corpus_lines + 1
+
+            # if last row in file is not empty
+            if self.all_docs[-1] != doc:
+                self.all_docs.append(doc)
+                self.sample_to_doc.pop()
+
             self.num_docs = len(self.all_docs)
 
         # load samples later lazily from disk
         else:
-            with open(corpus_path, "r", encoding=encoding) as f:
-                for line in tqdm(f, desc="Loading Dataset"):
-                    self.num_docs += 1
+            if self.corpus_lines is None:
+                with open(corpus_path, "r", encoding=encoding) as f:
+                    self.corpus_lines = 0
+                    for line in tqdm(f, desc="Loading Dataset", total=corpus_lines):
+                        if line.strip() == "":
+                            self.num_docs += 1
+                        else:
+                            self.corpus_lines += 1
+
+                    # if doc does not end with empty line
+                    if line.strip() != "":
+                        self.num_docs += 1
+
             self.file = open(corpus_path, "r", encoding=encoding)
+            self.random_file = open(corpus_path, "r", encoding=encoding)
 
     def __len__(self):
-        return self.num_docs
+        # last line of doc won't be used, because there's no "nextSentence". Additionally, we start counting at 0.
+        return self.corpus_lines - self.num_docs - 1
 
     def __getitem__(self, item):
         cur_id = self.sample_counter
@@ -93,29 +127,123 @@ class BERTDataset(Dataset):
                 self.file.close()
                 self.file = open(self.corpus_path, "r", encoding=self.encoding)
 
-        t1 = self.all_docs[item]
+        t1, t2, is_next_label = self.random_sent(item)
 
         # tokenize
         tokens_a = self.tokenizer.tokenize(t1)
+        tokens_b = self.tokenizer.tokenize(t2)
 
         # combine to one sample
-        cur_example = InputExample(guid=item, tokens_a=tokens_a)
+        cur_example = InputExample(guid=cur_id, tokens_a=tokens_a, tokens_b=tokens_b, is_next=is_next_label)
 
         # transform sample to features
         cur_features = convert_example_to_features(cur_example, self.seq_len, self.tokenizer)
 
         cur_tensors = (torch.tensor(cur_features.input_ids),
                        torch.tensor(cur_features.input_mask),
-                       torch.tensor(cur_features.lm_label_ids))
+                       torch.tensor(cur_features.segment_ids),
+                       torch.tensor(cur_features.lm_label_ids),
+                       torch.tensor(cur_features.is_next))
 
         return cur_tensors
 
+    def random_sent(self, index):
+        """
+        Get one sample from corpus consisting of two sentences. With prob. 50% these are two subsequent sentences
+        from one doc. With 50% the second sentence will be a random one from another doc.
+        :param index: int, index of sample.
+        :return: (str, str, int), sentence 1, sentence 2, isNextSentence Label
+        """
+        t1, t2 = self.get_corpus_line(index)
+        if random.random() > 0.5:
+            label = 0
+        else:
+            t2 = self.get_random_line()
+            label = 1
+
+        assert len(t1) > 0
+        assert len(t2) > 0
+        return t1, t2, label
+
+    def get_corpus_line(self, item):
+        """
+        Get one sample from corpus consisting of a pair of two subsequent lines from the same doc.
+        :param item: int, index of sample.
+        :return: (str, str), two subsequent sentences from corpus
+        """
+        t1 = ""
+        t2 = ""
+        assert item < self.corpus_lines
+        if self.on_memory:
+            sample = self.sample_to_doc[item]
+            t1 = self.all_docs[sample["doc_id"]][sample["line"]]
+            t2 = self.all_docs[sample["doc_id"]][sample["line"]+1]
+            # used later to avoid random nextSentence from same doc
+            self.current_doc = sample["doc_id"]
+            return t1, t2
+        else:
+            if self.line_buffer is None:
+                # read first non-empty line of file
+                while t1 == "" :
+                    t1 = self.file.__next__().strip()
+                    t2 = self.file.__next__().strip()
+            else:
+                # use t2 from previous iteration as new t1
+                t1 = self.line_buffer
+                t2 = self.file.__next__().strip()
+                # skip empty rows that are used for separating documents and keep track of current doc id
+                while t2 == "" or t1 == "":
+                    t1 = self.file.__next__().strip()
+                    t2 = self.file.__next__().strip()
+                    self.current_doc = self.current_doc+1
+            self.line_buffer = t2
+
+        assert t1 != ""
+        assert t2 != ""
+        return t1, t2
+
+    def get_random_line(self):
+        """
+        Get random line from another document for nextSentence task.
+        :return: str, content of one line
+        """
+        # Similar to original tf repo: This outer loop should rarely go for more than one iteration for large
+        # corpora. However, just to be careful, we try to make sure that
+        # the random document is not the same as the document we're processing.
+        for _ in range(10):
+            if self.on_memory:
+                rand_doc_idx = random.randint(0, len(self.all_docs)-1)
+                rand_doc = self.all_docs[rand_doc_idx]
+                line = rand_doc[random.randrange(len(rand_doc))]
+            else:
+                rand_index = random.randint(1, self.corpus_lines if self.corpus_lines < 1000 else 1000)
+                #pick random line
+                for _ in range(rand_index):
+                    line = self.get_next_line()
+            #check if our picked random line is really from another doc like we want it to be
+            if self.current_random_doc != self.current_doc:
+                break
+        return line
+
+    def get_next_line(self):
+        """ Gets next line of random_file and starts over when reaching end of file"""
+        try:
+            line = self.random_file.__next__().strip()
+            #keep track of which document we are currently looking at to later avoid having the same doc as t1
+            if line == "":
+                self.current_random_doc = self.current_random_doc + 1
+                line = self.random_file.__next__().strip()
+        except StopIteration:
+            self.random_file.close()
+            self.random_file = open(self.corpus_path, "r", encoding=self.encoding)
+            line = self.random_file.__next__().strip()
+        return line
 
 
 class InputExample(object):
     """A single training/test example for the language model."""
 
-    def __init__(self, guid, tokens_a, lm_labels=None):
+    def __init__(self, guid, tokens_a, tokens_b=None, is_next=None, lm_labels=None):
         """Constructs a InputExample.
 
         Args:
@@ -129,15 +257,19 @@ class InputExample(object):
         """
         self.guid = guid
         self.tokens_a = tokens_a
+        self.tokens_b = tokens_b
+        self.is_next = is_next  # nextSentence
         self.lm_labels = lm_labels  # masked words for language model
 
 
 class InputFeatures(object):
     """A single set of features of data."""
 
-    def __init__(self, input_ids, input_mask, lm_label_ids):
+    def __init__(self, input_ids, input_mask, segment_ids, is_next, lm_label_ids):
         self.input_ids = input_ids
         self.input_mask = input_mask
+        self.segment_ids = segment_ids
+        self.is_next = is_next
         self.lm_label_ids = lm_label_ids
 
 
@@ -190,14 +322,16 @@ def convert_example_to_features(example, max_seq_length, tokenizer):
     :return: InputFeatures, containing all inputs and labels of one sample as IDs (as used for model training)
     """
     tokens_a = example.tokens_a
+    tokens_b = example.tokens_b
     # Modifies `tokens_a` and `tokens_b` in place so that the total
     # length is less than the specified length.
     # Account for [CLS], [SEP], [SEP] with "- 3"
-    tokens_a = tokens_a[:max_seq_length - 3]
+    _truncate_seq_pair(tokens_a, tokens_b, max_seq_length - 3)
 
     t1_random, t1_label = random_word(tokens_a, tokenizer)
+    t2_random, t2_label = random_word(tokens_b, tokenizer)
     # concatenate lm labels and account for CLS, SEP, SEP
-    lm_label_ids = ([-1] + t1_label + [-1])
+    lm_label_ids = ([-1] + t1_label + [-1] + t2_label + [-1])
 
     # The convention in BERT is:
     # (a) For sequence pairs:
@@ -220,9 +354,19 @@ def convert_example_to_features(example, max_seq_length, tokenizer):
     tokens = []
     segment_ids = []
     tokens.append("[CLS]")
+    segment_ids.append(0)
     for token in tokens_a:
         tokens.append(token)
+        segment_ids.append(0)
     tokens.append("[SEP]")
+    segment_ids.append(0)
+
+    assert len(tokens_b) > 0
+    for token in tokens_b:
+        tokens.append(token)
+        segment_ids.append(1)
+    tokens.append("[SEP]")
+    segment_ids.append(1)
 
     input_ids = tokenizer.convert_tokens_to_ids(tokens)
 
@@ -234,10 +378,12 @@ def convert_example_to_features(example, max_seq_length, tokenizer):
     while len(input_ids) < max_seq_length:
         input_ids.append(0)
         input_mask.append(0)
+        segment_ids.append(0)
         lm_label_ids.append(-1)
 
     assert len(input_ids) == max_seq_length
     assert len(input_mask) == max_seq_length
+    assert len(segment_ids) == max_seq_length
     assert len(lm_label_ids) == max_seq_length
 
     if example.guid < 5:
@@ -247,11 +393,16 @@ def convert_example_to_features(example, max_seq_length, tokenizer):
                 [str(x) for x in tokens]))
         logger.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
         logger.info("input_mask: %s" % " ".join([str(x) for x in input_mask]))
+        logger.info(
+                "segment_ids: %s" % " ".join([str(x) for x in segment_ids]))
         logger.info("LM label: %s " % (lm_label_ids))
+        logger.info("Is next sentence label: %s " % (example.is_next))
 
     features = InputFeatures(input_ids=input_ids,
                              input_mask=input_mask,
-                             lm_label_ids=lm_label_ids)
+                             segment_ids=segment_ids,
+                             lm_label_ids=lm_label_ids,
+                             is_next=example.is_next)
     return features
 
 
@@ -434,7 +585,7 @@ def main():
             #TODO: check if this works with current data generator from disk that relies on file.__next__
             # (it doesn't return item back by index)
             train_sampler = DistributedSampler(train_dataset)
-        train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=args.train_batch_size, shuffle=True)
+        train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=args.train_batch_size)
 
         model.train()
         for _ in trange(int(args.num_train_epochs), desc="Epoch"):
@@ -471,6 +622,22 @@ def main():
         if args.do_train:
             torch.save(model_to_save.state_dict(), output_model_file)
 
+
+def _truncate_seq_pair(tokens_a, tokens_b, max_length):
+    """Truncates a sequence pair in place to the maximum length."""
+
+    # This is a simple heuristic which will always truncate the longer sequence
+    # one token at a time. This makes more sense than truncating an equal percent
+    # of tokens from each, since if one sequence is very short then each token
+    # that's truncated likely contains more information than a longer sequence.
+    while True:
+        total_length = len(tokens_a) + len(tokens_b)
+        if total_length <= max_length:
+            break
+        if len(tokens_a) > len(tokens_b):
+            tokens_a.pop()
+        else:
+            tokens_b.pop()
 
 
 def accuracy(out, labels):
